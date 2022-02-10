f6256acd08d1fd4d1c55b205d60e5f543b868e8d
MaximBaranovv <maksym.baranov1@nure.ua>
2022-02-10 13:49:59 +0200

    updating project

--- hm_2_basic/src/main/java/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-javac -cp ./libs/commons-lang3-3.12.0.jar:. ua/com/alevel/workclasses/TimeCalculation.java ua/com/alevel/Main.java
-
-java -cp ./libs/commons-lang3-3.12.0.jar:. ua.com.alevel.Main

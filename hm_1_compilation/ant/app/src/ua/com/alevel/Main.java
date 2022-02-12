39f5a060f2b1306956f9bf1ae06077c943130645
MaximBaranovv <maksym.baranov1@nure.ua>
2022-02-12 11:52:08 +0200

    updating project

---
+++ hm_1_compilation/ant/app/src/ua/com/alevel/Main.java
@@ -0,0 +1,21 @@
+package ua.com.alevel;
+
+import ua.com.alevel.test.PrintCommons;
+import ua.com.alevel.test.PrintString;
+import ua.com.alevel.test.PrintRandomInt;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PrintString class1 = new PrintString();
+        class1.printUC();
+        PrintRandomInt class2 = new PrintRandomInt();
+        class2.methodTwo();
+        PrintCommons class3 = new PrintCommons();
+        class3.printThree();
+
+
+
+
+    }
+}

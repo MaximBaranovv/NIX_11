865627fe1069ed431e8acd26ca069ca7a7572d79
MaximBaranovv <maksym.baranov1@nure.ua>
2022-03-11 19:50:21 +0200

    updating project

--- hm_2_base_type/run.sh
+++ hm_2_base_type/run.sh
@@ -3,4 +3,4 @@
 mvn clean
 rm dependency-reduced-pom.xml
 mvn install
-java -jar target/hm_2_base_types.jar
+java -jar target/hm_2_base_type.jar
--- hm_2_base_type/src/main/java/ua/com/alevel/Main.java
+++ hm_2_base_type/src/main/java/ua/com/alevel/Main.java
@@ -1,12 +1,7 @@
 package ua.com.alevel;
 
-import ua.com.alevel.workclasses.StringDetection;
-import ua.com.alevel.workclasses.SumOfNumbs;
-import ua.com.alevel.workclasses.TimeCalculation;
 import ua.com.alevel.workclasses.UserInterface;
-
 import java.io.IOException;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
